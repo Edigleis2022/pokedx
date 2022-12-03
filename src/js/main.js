b8a70ef5134086039ea090efc56baff3c32acb57
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2022-12-02 23:12:01 -0400

    Eventos Adicionados

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
 import '../css/style.css'
+
+/* Selecionar os Elementos no DOM */
+const inputPesquisa = document.querySelector("#inputPesquisa")
+const btnLocalizar = document.querySelector("#btnLocalizar")
+const pokedexDisplay = document.querySelector("#display")
+
+/* Adicionar Eventos */
+btnLocalizar.addEventListener('click',async function(){
+    /* buscar os dados dp pokemon na api */
+     const dadosDoPokemon = await localizarPokemon(inputPesquisa.value)
+    /*criar o cartâo do Pokemon */
+    criarCard(dadosDoPokemon)
+})
+
+async function localizarPokemon(termoBusca){
+    const url = `https://pokeapi.co/api/v2/pokemon/${termoBusca}`
+    const response = await fetch(url)
+    const pokemon = await response.json()
+    
+    return pokemon
+}
+
+function criarCard(pokemon){
+    const cartaoPokemon = document.createElement('div')
+    cartaoPokemon.className ='cartaoPokemon'
+    cartaoPokemon.innerHTML=`
+        <img class"pokemonSprite" src="${pokemon.sprites.front_default}"/>
+        <h2>${pokemon.name}<h2/>
+    `
+    pokedexDisplay.appendChild(cartaoPokemon)
+}
